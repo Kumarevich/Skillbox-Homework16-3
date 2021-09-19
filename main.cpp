e7ea7fa0a06e01134fa6b69f8039c7d3dec320d5
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-09-20 00:31:22 +0500

    used the stringstream

--- main.cpp
+++ main.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
     std::string strInput, number1str = "", number2str = "";
     std::cout << "Input your mathematical expression in format <number1><action><number2>: ";
     std::cin >> strInput;
-    double result;
+    std::stringstream ss;
+    ss << strInput;
+    double result, first, second;
+    char action;
 
-    char action = '0';
+    ss >> first;
+    ss >> action;
+    ss >> second;
 
-    for (int i = 0; i < strInput.length(); ++i) {
-        if (strInput[i] == '+' || strInput[i] == '-' || strInput[i] == '/' || strInput[i] == '*') {
-            action = strInput[i];
-        } else {
-            if (action == '0') {
-                number1str += strInput[i];
-            } else {
-                number2str += strInput[i];
-            }
-        }
-    }
-
-    double number1d = std::stod(number1str);
-    double number2d = std::stod(number2str);
     if (action == '+') {
-        result = number1d + number2d;
-    }
-    if (action == '-') {
-        result = number1d - number2d;
-    }
-    if (action == '/') {
-        result = number1d / number2d;
-    }
-    if (action == '*') {
-        result = number1d * number2d;
+        result = first + second;
+    } else if (action == '-') {
+        result = first - second;
+    } else if (action == '/') {
+        result = first / second;
+    } else if (action == '*') {
+        result = first * second;
     }
     std::cout << result;
     return 0;
